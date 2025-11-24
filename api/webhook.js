859325afcef95cc932dd297ec8c86a516df7ad0b
tomlovejerry1222 <tomlovejerry1222@gmail.com>
2025-11-24 17:18:16 +0800

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -294,7 +294,8 @@ export default async function handler(req, res) {
 
         if (results.length === 0) {
           const flexMap = buildFlexMessageFromNoResults();
-          await replyToLine(replyToken, flexMap, { type: "text", text: "抱歉，附近沒有符合條件的建築資料。" });
+          await replyToLine(replyToken, flexMap);
+          await replyToLine(replyToken, { type: "text", text: "抱歉，附近沒有符合條件的建築資料。" });
         } else {
           const flex = buildFlexMessageFromResults(results);
           await replyToLine(replyToken, flex);
